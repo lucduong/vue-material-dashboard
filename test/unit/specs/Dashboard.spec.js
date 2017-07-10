16fe1d1fe36d0c231fd4458477d6e5d1fd8995eb
Luc <luc@ltv.vn>
2017-07-10 23:14:28 +0700

    Edit Unit Test

--- test/unit/specs/Dashboard.spec.js
+++ test/unit/specs/Dashboard.spec.js
@@ -1,11 +1,30 @@
 import Vue from 'vue'
 import Dashboard from '@/components/Dashboard'
+import Chartist from 'chartist'
+import CoreComponents from '@/core/components'
+
+Vue.use(CoreComponents)
+
+function getRenderedComponent(Component, propsData) {
+  Vue.mixin({
+    beforeCreate() {
+      this._Chartist = Chartist
+    }
+  })
+
+  Object.defineProperty(Vue.prototype, '$Chartist', {
+    get() { return this.$root._Chartist }
+  })
+
+  return mount(Component, propsData) // eslint-disable-line
+}
 
 describe('Dashboard.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(Dashboard)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelectorAll('.row').length)
-      .to.equal(3)
+  it('should contains 4 stats cards and 3 chart cards', () => {
+    const vm = getRenderedComponent(Dashboard)
+    var statsCards = vm.$children.filter((child) => { child.$options.name === 'md-widget' }).length
+    var chartCards = vm.$children.filter((child) => child.$options.name === 'md-chart-widget').length
+    expect(statsCards).to.equal(4)
+    expect(chartCards).to.equal(3)
   })
 })
