3ca2d890c85bba011c146ed49eef347ce644cba3
Victor Villalobos <victorvillalobos@outlook.com>
2017-07-26 02:59:52 -0400

    Fix v-model directive in textarea component

--- src/core/components/mdFormGroupInput/index.vue
+++ src/core/components/mdFormGroupInput/index.vue
@@ -2,7 +2,7 @@
 .form-group(:class="{ 'label-floating' : labelFloating }")
   label.control-label(v-if='label')
     | {{label}}
-  textarea.form-control(v-bind='$props', @input="$emit('input',$event.target.value)", v-if="type === 'textarea'")
+  textarea.form-control(v-bind='$props', @input="$emit('input',$event.target.value)" , v-if="type === 'textarea'")
   input.form-control(v-bind='$props', :value='value', @input="$emit('input',$event.target.value)", v-else)
 </template>
 <script>
